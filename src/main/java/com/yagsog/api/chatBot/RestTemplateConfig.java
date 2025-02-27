84d5a3904a37babdb92e2816d475b668e8789897
daisy.lee <zzeonii0612@gmail.com>
2025-02-27 21:46:09 +0900

    Feat: ChatBot RestTemplate 수정

---
+++ src/main/java/com/yagsog/api/chatBot/RestTemplateConfig.java
@@ -0,0 +1,14 @@
+package com.yagsog.api.chatBot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
--- src/test/java/com/yagsog/api/ApiApplicationTests.java
+++ src/test/java/com/yagsog/api/ApiApplicationTests.java
@@ -1,12 +1,6 @@
 package com.yagsog.api;
-
-import com.yagsog.api.chatBot.Chatbot;
-import com.yagsog.api.chatBot.ChatbotService;
-import com.yagsog.api.user.User;
-import com.yagsog.api.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -15,9 +9,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest(classes = ApiApplication.class)
 @ActiveProfiles("test")
 class ApiApplicationTests {
-
-	@Autowired
-	private UserService userService;
 
 	@Test
 	void contextLoads() {
