54f7e64b9b6c1b4e9dcd935fac89b7ea4512075f
daisy.lee <zzeonii0612@gmail.com>
2025-02-27 21:16:34 +0900

    Add: Test code add

--- src/test/java/com/yagsog/api/ApiApplicationTests.java
+++ src/test/java/com/yagsog/api/ApiApplicationTests.java
@@ -1,13 +1,27 @@
 package com.yagsog.api;
 
+import com.yagsog.api.chatBot.Chatbot;
+import com.yagsog.api.chatBot.ChatbotService;
+import com.yagsog.api.user.User;
+import com.yagsog.api.user.UserService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiApplicationTests {
+
+	@Autowired
+	private UserService userService;
 
 	@Test
 	void contextLoads() {
+		//
 	}
 
 }
